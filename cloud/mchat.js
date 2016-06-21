55fcc524d3d08f0cc97aec6057315e1ccdd62bae
NobLee <liang554166037@163.com>
2016-06-21 16:30:29 +0800

    5

--- cloud/mchat.js
+++ cloud/mchat.js
@@ -30,7 +30,7 @@ function getMsgDesc(msg) {
 }
 
 function receiversOffline(req, res) {
-  console.log("接受人" + JSON.stringify(req.offlinePeers) + "*" + JSON.stringify(res.offlinePeers);
+  console.log("接受人" + JSON.stringify(req.offlinePeers) + "*******" + JSON.stringify(res.offlinePeers);
 
   if (req.params.convId) {
     // api v2
